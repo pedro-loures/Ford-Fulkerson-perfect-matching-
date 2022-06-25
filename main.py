08032dd63b572a4e73861db0df1d00f37d81e593
pedro-loures <PLoures2@hotmail.com>
2022-06-25 20:16:55 -0300

    python file structure

---
+++ main.py
@@ -0,0 +1,27 @@
+# AUTHORS 
+# Pedro Loures Alzamora
+# Description: implementation of ford fulkerson with bfs to generate perfect matchings in a bipartite grapgh
+# TODO LIST
+# -0 TODO implement graph structure
+# -1 TODO Read entry
+# -2 TODO implement bfs
+# -3 TODO implement ford fulkerson
+# -4 TODO check perfect matching
+# -5 TODO output padronization
+
+# External imports
+import numpy as np
+import scipy
+
+# Local imports
+import ford_fulkerson as ff
+import graph_search as gs
+import utils as ut
+
+
+def main():
+  pass
+
+
+if __name__ == "__main__":
+    main()
